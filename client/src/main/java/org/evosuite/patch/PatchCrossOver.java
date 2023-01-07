8d9300029a75a10950ef1dd08e2cdd414b02c4e6
crhf <ruanhf8@gmail.com>
2023-01-07 17:42:03 +0800

    implement PatchCrossOver

--- client/src/main/java/org/evosuite/patch/PatchCrossOver.java
+++ client/src/main/java/org/evosuite/patch/PatchCrossOver.java
@@ -2,6 +2,7 @@ package org.evosuite.patch;
 
 import org.evosuite.ga.ConstructionFailedException;
 import org.evosuite.ga.operators.crossover.CrossOverFunction;
+import org.evosuite.utils.Randomness;
 
 public class PatchCrossOver extends CrossOverFunction<PatchChromosome> {
 
@@ -14,6 +15,32 @@ public class PatchCrossOver extends CrossOverFunction<PatchChromosome> {
      */
     @Override
     public void crossOver(PatchChromosome parent1, PatchChromosome parent2) throws ConstructionFailedException {
+        // HUXSinglePointCrossover in Arja
+        int size = parent1.bits.size();
 
+        for (int i = 0; i < size; i++) {
+            if (parent1.bits.get(i) != parent2.bits.get(i)) {
+                if (Randomness.nextDouble() < 0.5) {
+                    // swap two bits
+                    parent1.bits.flip(i);
+                    parent2.bits.flip(i);
+                }
+            }
+        }
+
+        int arrayCrossOverPoint1 = Randomness.nextInt(0, size);
+        for (int i = arrayCrossOverPoint1; i < size; i++) {
+            int tmp = parent1.array[i];
+            parent1.array[i] = parent2.array[i];
+            parent2.array[i] = tmp;
+        }
+
+        int doubleSize = size + size;
+        int arrayCrossOverPoint2 = Randomness.nextInt(size + 1, doubleSize);
+        for (int i = arrayCrossOverPoint2; i < doubleSize; i++) {
+            int tmp = parent1.array[i];
+            parent1.array[i] = parent2.array[i];
+            parent2.array[i] = tmp;
+        }
     }
 }
