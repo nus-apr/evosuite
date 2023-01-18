40bfabe9df44ca5d0a16036be6ebe87ddf4a1b84
Hoang Lam Nguyen <hoang.lam.nguyen@hu-berlin.de>
2023-01-18 20:31:01 +0800

    add math58 test for debugging

--- master/src/test/java/org/evosuite/abc/Defects4JSystemTest.java
+++ master/src/test/java/org/evosuite/abc/Defects4JSystemTest.java
@@ -15,9 +15,7 @@ public class Defects4JSystemTest extends SystemTestBase {
 
 
     /**
-     * CLI command: java -jar master/target/evosuite-master-1.2.0.jar -class org.apache.commons.math3.distribution.HypergeometricDistribution
-     * -generateMOSuite -evorepair testgen -targetPatches math2_patches.json
-     * -projectCP /home/lam/workspace/nus-apr/evoRepair/test/math_2_instr/target/classes -Dminimize=false -Dassertions=false
+     * CLI command: java -jar master/target/evosuite-master-1.2.0.jar -class org.apache.commons.math3.distribution.HypergeometricDistribution -generateMOSuite -evorepair testgen -targetPatches math2_patches.json -projectCP /home/lam/workspace/nus-apr/evoRepair/test/math_2_instr/target/classes -Dminimize=false -Dassertions=false
      */
     @Test
     public void testMath2() {
@@ -28,10 +26,34 @@ public class Defects4JSystemTest extends SystemTestBase {
 
         String targetClass = "org.apache.commons.math3.distribution.HypergeometricDistribution";
         String projectCP = "/home/lam/workspace/nus-apr/evoRepair/test/math_2_instr/target/classes";
-        //Properties.JUNIT_SUFFIX = "_Debug_ESTest";
-        //Properties.JUNIT_TESTS = true; // Write test suite to out.
+        Properties.JUNIT_SUFFIX = "_Debug_ESTest";
+        Properties.JUNIT_TESTS = true; // Write test suite to out.
         String[] command = new String[] {"-generateMOSuite", "-evorepair", "testgen", "-targetPatches", resource.getPath(), "-class", targetClass,
         "-projectCP", projectCP};
+
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<TestSuiteChromosome> ga = getGAFromResult(result);
+    }
+
+    /**
+     * FIX: Target line goals for private methods
+     * CLI cmd: java -jar master/target/evosuite-master-1.2.0.jar -class org.apache.commons.math.optimization.fitting.GaussianFitter -generateMOSuite -evorepair testgen -targetPatches ../evorepair-experiments/evorepair/C1-evorepair-defects4j-Math-58-6c757b79/output/math_58-math_58-230118_170804/gen-test/target_patches_gen1.json -projectCP ../defects4j-instrumented/instrumented-archives/math_58/target/classes -Dminimize=false -Dassertions=false
+     */
+    @Test
+    public void testMath58() {
+
+        EvoSuite evosuite = new EvoSuite();
+
+        URL resource = this.getClass().getResource("math2_patches.json");
+
+        String targetClass = "org.apache.commons.math.optimization.fitting.GaussianFitter";
+        String targetPatches = "/home/lam/workspace/nus-apr/evorepair-experiments/evorepair/C1-evorepair-defects4j-Math-58-6c757b79/output/math_58-math_58-230118_170804/gen-test/target_patches_gen1.json";
+        String projectCP = "/home/lam/workspace/nus-apr/defects4j-instrumented/instrumented-archives/math_58/target/classes";
+        //Properties.JUNIT_SUFFIX = "_Debug_ESTest";
+        //Properties.JUNIT_TESTS = true; // Write test suite to out.
+        String[] command = new String[] {"-generateMOSuite", "-evorepair", "testgen", "-targetPatches", targetPatches, "-class", targetClass,
+                "-projectCP", projectCP};
 
 
         Object result = evosuite.parseCommandLine(command);
