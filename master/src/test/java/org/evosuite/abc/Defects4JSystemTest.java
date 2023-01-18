dea00194850acc60038caf0106d48145da7fd013
Hoang Lam Nguyen <hoang.lam.nguyen@hu-berlin.de>
2023-01-18 11:14:19 +0800

    Add base defects4j system test

--- master/src/test/java/org/evosuite/abc/Defects4JSystemTest.java
+++ master/src/test/java/org/evosuite/abc/Defects4JSystemTest.java
@@ -18,31 +18,15 @@ public class Defects4JSystemTest extends SystemTestBase {
 
         EvoSuite evosuite = new EvoSuite();
 
-        URL resource = this.getClass().getResource("patch_population.json");
+        URL resource = this.getClass().getResource("math2_patches.json");
 
-        String clazz = "org.apache.commons.math3.distribution.HypergeometricDistribution";
-        //String clazz = "org.apache.commons.math3.distribution.HypergeometricDistribution";
+        String targetClass = "org.apache.commons.math3.distribution.HypergeometricDistribution";
         String projectCP = "/home/lam/workspace/nus-apr/evoRepair/test/math_2_instr/target/classes";
-        Properties.JUNIT_SUFFIX = "_gen_2_ESTest";
+        Properties.JUNIT_SUFFIX = "_Debug_ESTest";
+        Properties.JUNIT_TESTS = true; // Write test suite to out.
+        String[] command = new String[] {"-generateMOSuite", "-evorepair", "testgen", "-targetPatches", resource.getPath(), "-class", targetClass,
+        "-projectCP", projectCP};
 
-        //String[] command = new String[] {"-generateMOSuite", "-targetLines", resource.getPath(),"-port", "7777", "-class", targetClass };
-        //String[] command = new String[] {"-generateMOSuite", "-evorepair", "testgen", "-targetPatches", resource.getPath(), "-port", port, "-class", targetClass,
-        //"-projectCP", projectCP, "-base_dir", base_dir};
-
-        Properties.ASSERTIONS = false;
-        Properties.ALGORITHM = Properties.Algorithm.MOSAPATCH;
-        Properties.CRITERION = new Properties.Criterion[]{
-                PATCH, PATCHLINE
-        };
-        Properties.TIMEOUT=100_000;
-        Properties.GLOBAL_TIMEOUT=100_000;
-        Properties.MOCK_IF_NO_GENERATOR = false;
-        // FIXME: Test suite minimization seems to break the number of covered goals in the stats
-
-        // Debugging
-        Properties.MINIMIZE = false;
-        Properties.EXTRA_TIMEOUT=Integer.MAX_VALUE;
-        Properties.MINIMIZATION_TIMEOUT=Integer.MAX_VALUE;
 
         Object result = evosuite.parseCommandLine(command);
         GeneticAlgorithm<TestSuiteChromosome> ga = getGAFromResult(result);
