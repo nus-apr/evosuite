0603beabba4864a40841ac98fa16a50ed6ba1542
Hoang Lam Nguyen <hoang.lam.nguyen@hu-berlin.de>
2023-01-24 18:07:02 +0800

    Add custom exception test

--- master/src/test/java/com/examples/with/different/packagename/coverage/ClassWithCustomException.java
+++ master/src/test/java/com/examples/with/different/packagename/coverage/ClassWithCustomException.java
@@ -2,25 +2,29 @@ package com.examples.with.different.packagename.coverage;
 
 public class ClassWithCustomException {
 
-    public char test(String s) {
-        try {
-            return handleString(s);
-        } catch (StringIndexOutOfBoundsException e) {
-            if (s.contains("&")) {
-                throw new RuntimeException("[Defects4J_BugReport_Violation]");
-            } else {
-                throw e;
-            }
-        }
-    }
-
-    // Return the first char following '&', which will throw an exception if it is the only char
-    private char handleString(String s) {
-        if (s.startsWith("&")) {
-            return s.charAt(s.indexOf("&") + 1);
+    public int testInt(int x) {
+        if  (x > 1024) {
+            throw new RuntimeException("[Defects4J_BugReport_Violation]");
         } else {
-            throw new IllegalArgumentException("Input does not contain char &"); // Expected exception
+            return x;
         }
     }
 
+    /*
+    public char test(String s) {
+        return s.charAt(0);
+    }
+
+
+    public int other(String s) {
+        if (s.length() > 1) {
+            if (s.endsWith("&")) {
+                throw new RuntimeException("[Defects4J_BugReport_Violation]");
+            }
+            return 0;
+        }
+        return 1;
+    }
+
+     */
 }
--- master/src/test/java/org/evosuite/abc/CustomExceptionSystemTest.java
+++ master/src/test/java/org/evosuite/abc/CustomExceptionSystemTest.java
@@ -17,13 +17,13 @@ import java.util.Arrays;
 public class CustomExceptionSystemTest extends SystemTestBase {
 
     @Test
-    public void testCustomException() {
+    public void testWholeSuiteCustomException() {
         EvoSuite evosuite = new EvoSuite();
         String targetClass = ClassWithCustomException.class.getCanonicalName();
         Properties.TARGET_CLASS = targetClass;
         URL resource = this.getClass().getResource("patch_population_custom_exception.json");
 
-        String[] command = new String[] {"-evorepair", "testgen", "-generateMOSuite", "-criterion", "PATCHLINE:METHODNOEXCEPTION", "-targetPatches", resource.getPath(), "-class", targetClass };
+        String[] command = new String[] {"-evorepair", "testgen", "-generateSuite", "-criterion", "PATCHLINE:EXCEPTION", "-targetPatches", resource.getPath(), "-class", targetClass };
 
 
         Object result = evosuite.parseCommandLine(command);
@@ -34,9 +34,7 @@ public class CustomExceptionSystemTest extends SystemTestBase {
         int goals = TestGenerationStrategy.getFitnessFactories().get(0).getCoverageGoals().size(); // assuming single fitness function
         Assert.assertEquals("Wrong number of goals: ", 1, goals);
 
-        // MOSATestSuiteAdapter.getBestIndividuals:95 sets the suite fitness to 1.0 for some reason, even if all goals have been covered
-        // TODO EvoRepair: investigate
-        Assert.assertEquals("Non-optimal fitness: ", 1.0, best.getFitness(), 0.01);
+        Assert.assertEquals("Non-optimal fitness: ", 0, best.getFitness(), 0.01);
     }
 
     @Test
