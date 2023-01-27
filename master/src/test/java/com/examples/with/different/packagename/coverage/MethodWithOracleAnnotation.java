b155c7aea1171785864cb410cf1be007d3a36def
Hoang Lam Nguyen <hoang.lam.nguyen@hu-berlin.de>
2023-01-27 17:37:16 +0800

    WIP

---
+++ master/src/test/java/com/examples/with/different/packagename/coverage/MethodWithOracleAnnotation.java
@@ -0,0 +1,34 @@
+package com.examples.with.different.packagename.coverage;
+
+public class MethodWithOracleAnnotation {
+
+    public void topLevelMethod(boolean a, int x) {
+        if (a) {
+            intermediateMethod(x);
+        } else {
+            intermediateMethod(0);
+        }
+    }
+
+    public void intermediateMethod(int x) {
+        StringBuilder input = new StringBuilder();
+        for (int i = 0; i < x; i++) {
+            input.append(i);
+        }
+        instrumentedMethod(input.toString());
+    }
+
+    public void instrumentedMethod(String input) {
+        try {
+            buggyMethod(input);
+        } catch (StringIndexOutOfBoundsException e) {
+            if (input.isEmpty()) {
+                throw new RuntimeException("[Defects4J_BugReport_Violation]");
+            }
+        }
+    }
+
+    public int buggyMethod(String input) {
+        return input.charAt(0); // May return NPE or StringIndexOutOfBoundsException for ""
+    }
+}
---
+++ master/src/test/java/org/evosuite/abc/OracleIBranchSystemTest.java
@@ -0,0 +1,58 @@
+package org.evosuite.abc;
+
+import com.examples.with.different.packagename.coverage.BranchInsideCatchBlock;
+import com.examples.with.different.packagename.coverage.MethodWithOracleAnnotation;
+import org.evosuite.EvoSuite;
+import org.evosuite.Properties;
+import org.evosuite.SystemTestBase;
+import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.strategy.TestGenerationStrategy;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URL;
+
+public class OracleIBranchSystemTest extends SystemTestBase {
+
+    @Test
+    public void testOracleIBranchWithNSGAII() {
+        EvoSuite evosuite = new EvoSuite();
+        String targetClass = MethodWithOracleAnnotation.class.getCanonicalName();
+        URL oracleLocations = this.getClass().getResource("MethodWithOracleAnnotation_oracleLocations.json");
+        URL targetPatches = this.getClass().getResource("empty_patch_population.json");
+
+
+        String[] command = new String[] {"-evorepair", "testgen", "-generateSuite", "-class", targetClass, "-criterion", "IBRANCH",
+                "-oracleLocations", oracleLocations.getPath(), "-targetPatches",  targetPatches.getPath()};
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        //System.out.println("EvolvedTestSuite:\n" + best);
+
+        int goals = TestGenerationStrategy.getFitnessFactories().get(0).getCoverageGoals().size(); // assuming single fitness function
+        Assert.assertEquals("Wrong number of goals: ", 4, goals);
+        Assert.assertEquals("Non-optimal fitness: ", 0.0, best.getFitness(), 0.01);
+    }
+
+    @Test
+    public void testOracleIBranchWithMOSA() {
+        EvoSuite evosuite = new EvoSuite();
+        String targetClass = MethodWithOracleAnnotation.class.getCanonicalName();
+        URL oracleLocations = this.getClass().getResource("MethodWithOracleAnnotation_oracleLocations.json");
+        URL targetPatches = this.getClass().getResource("empty_patch_population.json");
+
+
+        String[] command = new String[] {"-evorepair", "testgen", "-generateMOSuite", "-class", targetClass, "-criterion", "IBRANCH",
+                "-oracleLocations", oracleLocations.getPath(), "-targetPatches",  targetPatches.getPath()};
+
+        Object result = evosuite.parseCommandLine(command);
+        GeneticAlgorithm<?> ga = getGAFromResult(result);
+        TestSuiteChromosome best = (TestSuiteChromosome) ga.getBestIndividual();
+        //System.out.println("EvolvedTestSuite:\n" + best);
+
+        int goals = TestGenerationStrategy.getFitnessFactories().get(0).getCoverageGoals().size(); // assuming single fitness function
+        Assert.assertEquals("Wrong number of goals: ", 4, goals);
+        Assert.assertEquals("Non-optimal fitness: ", 0.0, best.getFitness(), 0.01);
+    }
+}
