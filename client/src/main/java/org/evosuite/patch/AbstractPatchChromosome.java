cfc8767ef94cbc371dce5f75efff94e7052a2d10
crhf <ruanhf8@gmail.com>
2023-02-03 00:37:28 +0800

    set secondary objective of AbstractPatchChromosome

--- client/src/main/java/org/evosuite/patch/AbstractPatchChromosome.java
+++ client/src/main/java/org/evosuite/patch/AbstractPatchChromosome.java
@@ -1,6 +1,38 @@
 package org.evosuite.patch;
 
 import org.evosuite.ga.Chromosome;
+import org.evosuite.ga.FitnessFunction;
 
 public abstract class AbstractPatchChromosome<E extends AbstractPatchChromosome<E>>
-    extends Chromosome<E> implements Patch {}
+    extends Chromosome<E> implements Patch {
+
+    private boolean hasSizePatchFitness = false;
+
+    /**
+     * Adds a fitness function with an associated fitness value, coverage value,
+     * and number of covered goals.
+     *
+     * @param ff              a fitness function
+     * @param fitnessValue    the fitness value for {@code ff}
+     * @param coverage        the coverage value for {@code ff}
+     * @param numCoveredGoals the number of covered goals for {@code ff}
+     */
+    @Override
+    public void addFitness(FitnessFunction<E> ff, double fitnessValue, double coverage, int numCoveredGoals) {
+        super.addFitness(ff, fitnessValue, coverage, numCoveredGoals);
+        if (ff instanceof SizePatchFitness) {
+            hasSizePatchFitness = true;
+        }
+    }
+
+    /**
+     * Secondary Objectives are specific to chromosome types
+     *
+     * @param o a {@link Chromosome} object.
+     * @return a int.
+     */
+    @Override
+    public int compareSecondaryObjective(E o) {
+        return hasSizePatchFitness ? 0 : Double.compare(getSizePatchFitness(), o.getSizePatchFitness());
+    }
+}
